8ad5173577cf524603463b21e2d106f19a2a3d1c
PATIL NEHA <patilneha0518@gmail.com>
2026-07-22 23:21:31 +0530

    Time: 3 ms (96.13%), Space: 10.6 MB (99.34%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+// struct ListNode {
+//     int val;
+//     struct ListNode *next;
+// };
+
+struct ListNode *detectCycle(struct ListNode *head) {
+    if (head == NULL || head->next == NULL) return NULL;
+
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            struct ListNode* entry = head;
+            while (entry != slow) {
+                entry = entry->next;
+                slow = slow->next;
+            }
+            return entry; 
+        }
+    }
+    return NULL; 
+}
+
+struct ListNode* createNode(int val) {
+    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+    newNode->val = val;
+    newNode->next = NULL;
+    return newNode;
+}
